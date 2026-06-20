87b8edc81f57e87656bab8ff082812e7441acbc6
Y.K Kim <y2kkyk@nate.com>
2026-06-20 16:15:56 +0900

    #000029 feat(generator): add generator registry

---
+++ builder/generator/registry.py
@@ -0,0 +1,23 @@
+﻿from builder.generator.domain import DomainGenerator
+from builder.generator.project import ProjectGenerator
+
+
+class GeneratorRegistry:
+    def __init__(self):
+        self._generators = {}
+
+    def register(self, name, generator_class):
+        self._generators[name] = generator_class
+
+    def create(self, name):
+        if name not in self._generators:
+            raise Exception("Unknown generator")
+
+        return self._generators[name]()
+
+
+def default_registry():
+    registry = GeneratorRegistry()
+    registry.register("domain", DomainGenerator)
+    registry.register("project", ProjectGenerator)
+    return registry
---
+++ tests/test_generator_registry.py
@@ -0,0 +1,21 @@
+﻿from builder.generator.domain import DomainGenerator
+from builder.generator.project import ProjectGenerator
+from builder.generator.registry import GeneratorRegistry, default_registry
+
+
+def test_generator_registry_registers_and_creates_generator():
+    registry = GeneratorRegistry()
+
+    registry.register("domain", DomainGenerator)
+
+    generator = registry.create("domain")
+
+    assert isinstance(generator, DomainGenerator)
+
+
+def test_default_registry_creates_project_generator():
+    registry = default_registry()
+
+    generator = registry.create("project")
+
+    assert isinstance(generator, ProjectGenerator)
